4e636ac3296c4ef696662ca53ce3ece454d60146
rana reda roshdy <ranaredaroshdy.com>
2023-08-21 18:18:02 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,11 @@ int _strlen(char *s)
 {
 	int longi = 0;
 
-	while (*s != '\0')	
+	while (*s != '\0')
 	{
-            longi++;
-	    s++;
+		longi++;
+		s++;
 	}
-	
-        return (longi);
+
+	return (longi);
 }
