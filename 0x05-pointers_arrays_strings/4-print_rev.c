79627f764de854d2b9dca44da87e3afc00685a67
rana reda roshdy <ranaredaroshdy.com>
2023-08-22 02:21:07 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * print_rev - in reverse, followed by a new line
- * @s: print string
+ * print_rev - imprime en reversa
+ * @s: string
  * return: 0
  */
 void print_rev(char *s)
 {
-	int l = 0;
-	int i;
+	int longi = 0;
+	int o;
 
 	while (*s != '\0')
 	{
-	        s++;
-		l++;
-
+		longi++;
+		s++;
 	}
 	s--;
-	for (i = l;i > 0; i--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
-	}	
+	}
+
 	_putchar('\n');
 }
