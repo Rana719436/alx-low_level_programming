39cdf1e6b4ada1ef5cf4a101c266c8c4263f1864
rana reda roshdy <ranaredaroshdy.com>
2023-08-23 00:04:21 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,15 @@
  */
 void puts2(char *str)
 {
-        int i;
+        int i = 0;
 
 	while (str[i] != '\0')
-              if (i % 2 == 0)
+	{ 
+		if (i % 2 == 0) 
 	{
-                 i++;
- 		 i += 2;
 		 _putchar(str[i]);
+	}
+		 i += 2;
 	}
 	_putchar('\n');
 }
