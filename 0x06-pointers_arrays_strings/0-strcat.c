922bc8643ec2f6db083b733a7bfb7314b0d32d70
rana reda roshdy <ranaredaroshdy.com>
2023-08-24 01:39:53 -0700

    =====

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings,
+ * @dest: input value
+ * @src: input value
+ * Return: a pointer to dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+     int d = 0;
+     int s = 0;
+          
+     while (*(dest + d) != '\0')
+     {
+          d++;
+     }
+     while (*(src + s) != '\0')
+     { 
+          *(dest + d) = *(src + s);
+	  d++;
+	  s++;
+     }
+return (dest);
+}
