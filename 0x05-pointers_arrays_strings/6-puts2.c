0a2010e9aed51cf0b196605d1d11415d177c88c5
rana reda roshdy <ranaredaroshdy.com>
2023-08-24 17:05:18 -0700

    =====

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,23 @@
 #include "main.h"
+
 /**
- * puts2 - prints every other character of a string 
- * @str : input starting 
+ * puts2 - function that prints every other character
+ *
+ * @str: pointer to char
+ * Return: nothing
  */
+
 void puts2(char *str)
 {
-      int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-       	{
-          _putchar(str[i]);
-        }
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i += 1;
+	}
+	_putchar('\n');
 }
