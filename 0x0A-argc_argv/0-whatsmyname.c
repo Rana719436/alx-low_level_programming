d584ef955889b902fa0bd12a60b88523452a9716
Rana Reda Roshdy <ranaredaroshdy@gmail.com>
2023-09-01 05:22:42 -0700

    =====

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - prints its name, followed by a new line.
  * @argc: number of arguments.
  * @argv: array of arguments.
- * Return: Always 0 
+ * Return: Always 0 (Success) 
  */
 int main (int argc __attribute__((unused)), char *argv[])
 {
