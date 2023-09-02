2c1ed1892d80071d2e55bf4e2464a52874171a4e
Rana Reda Roshdy <ranaredaroshdy@gmail.com>
2023-09-01 18:07:52 -0700

    =====

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - Check for uppercase character
+* @g: The number to be checked
+* Return: 1 for uppercase letter or 0 otherwise
+*/
+
+int _isupper(int g)
+{
+	if (g >= 65 && g <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
