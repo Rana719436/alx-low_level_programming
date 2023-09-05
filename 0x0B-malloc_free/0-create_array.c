5726d38224c148fa00471b15f122eb5efad1107b
Rana Reda Roshdy <ranaredaroshdy@gmail.com>
2023-09-05 15:33:03 -0700

    your commit massage

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,18 +8,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-unsigned int i;
+char *str;
+	unsigned int i;
 
-	array = malloc(size);
-		if (size == 0 || array == NULL)
-		{
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
 		return (NULL);
-		}
 
-	for (i = 0; i > size; i++)
-	{
-		array[i] = c;
-	}
-	return (array);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
