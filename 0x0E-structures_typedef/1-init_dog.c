d0493d99d01bf956f94b27df71368f33c6c86e86
Rana Reda Roshdy <ranaredaroshdy@gmail.com>
2023-09-11 15:27:59 -0700

    =====

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog
+ * @d: the dog to init
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * description: just a long dog struct in big kitty world
+ **/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
