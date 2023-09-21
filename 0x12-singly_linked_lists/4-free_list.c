e11dd614e2ac0f9f2c72a524a709e9b8d42295af
rana reda roshdy <ranaredaroshdy@gmail.com>
2023-09-21 00:23:30 -0700

    lol

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ *  * free_list - a function that frees a list_t list.
+ *   * @head: Pointer to the start of the list.
+ *    * Return: EXIT_SUCCESS.
+     */
+void free_list(list_t *head)
+{
+	list_t *node = head;
+		/* free strings */
+	while (node)
+	{
+		free(node->str);
+		node = node->next;
+	}
+		/* free nodes */
+	while (head)
+	{
+		list_t *temp = head;
+
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ *  * get_last_node - a function that gets the last node of lists.
+ *   * @head: node head.
+ *    * Return: address of last element.
+     */
+list_t *get_last_node(list_t *head)
+{
+	list_t *last = head;
+if (!last)			
+	return (0);
+	}
+	while (last->next)
+	{
+		last = last->next;
+	}
+	return (last);
+}
