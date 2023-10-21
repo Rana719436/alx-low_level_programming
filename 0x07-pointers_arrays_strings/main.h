abbf1c8b4d27b41448da75bc81e80752efcf951f
rana reda roshdy <ranaredaroshdy@gmail.com>
2023-10-21 15:04:54 -0700

    lol

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * File: main.h
- *
- * Desc: headerfile
- **/
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -14,4 +8,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-#endif /* MAIN_H */
+void set_string(char **s, char *to);
+int _putchar(char c);
+#endif
